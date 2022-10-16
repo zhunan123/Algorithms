b40ee34e8c50a0c272d2c7c76707adfaa6a2b9c2
zhunan123 <zhunan@bu.edu>
2022-10-15 23:20:16 -0700

    add preorder and inorder traversal tree method

---
+++ BFSDFSTree/InOrderTraversal.java
@@ -0,0 +1,30 @@
+package BFSDFSTree;
+
+public class InOrderTraversal {
+   /**
+    * 输入
+    * 二叉树 = {1,2,3}
+    * 输出：
+    * [2,1,3]
+    * 解释：
+    *
+    *       1
+    *     /   \
+    *   2       3
+    * */
+
+   public List<Integer> inorderTraversal(TreeNode root) {
+       List<Integer> res = new ArrayList<Integer>();
+       inOrder(root, res);
+       return res;
+   }
+
+    public void inOrder(TreeNode root, List<Integer> res){
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, res);
+        res.add(root.val);
+        inOrder(root.right, res);
+    }
+}
