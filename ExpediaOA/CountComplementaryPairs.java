cbcdae3ac7a0639fe7785707f418c5c6e50b6fbb
zhunan123 <zhunan@bu.edu>
2022-10-29 14:03:20 -0700

    add expedia oa

--- ExpediaOA/CountComplementaryPairs.java
+++ ExpediaOA/CountComplementaryPairs.java
@@ -7,6 +7,21 @@ import java.util.Map;
 public class CountComplementaryPairs {
 
     public static long countCountComplementaryPairs(List<String> stringData) {
+        /**
+         * One of the shops in the HackerMall is offering discount coupons bassed on a puzzling problem. There
+         * are n tags where each tags has a value denoted by val[i]. a customer needs to choose the rags in such
+         * way that the sum of values is even.
+         * The goal is to find the maximum possible even sum of values of tags that can be chosen.
+         * Example:
+         * Tag values are val[2,3,6,-5,10,1,1]
+         * Consider some of the following chosen tags and theor corresponding sums:
+         *          chosen tags            sum          is sum even?
+         *          [2,3,6,10,1,1]         23            No
+         *          [2,3,6,-5,10,1,1]      18            Yes
+         *          [2,6,10,1,1]           20            Yes
+         *          [2,3,6,10,1]           22            Yes
+         * The tags[2,3,6,10,1] sum to 22 which is even and is the macimum possible, Hence the answer is 22;
+         * */
         Map<Integer, Integer> map = new HashMap<>();
         for (String s : stringData) {
             Integer res = 0;
--- ExpediaOA/GetMaximumEvenNumber.java
+++ ExpediaOA/GetMaximumEvenNumber.java
@@ -5,6 +5,20 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class GetMaximumEvenNumber {
+    /**
+     * A pair of string form a complementary pair if there is some permutation of their concatenation that is a
+     * palindrome, For example, the strings "abac" and "cab" form a complementary pair since their concatenation is "abaccab"
+     * which can be rearranged to from a palindrome, i.e "bcaaacb"
+     * Given an array of n strings, find the number of complementary pairs that can be formed.
+     * Example:
+     * consider stringData = ["abc", "abcd", "bc", "adc"]
+     * The following complementary pairs can be formed:
+     * 1.{"abc", "abcd"}, concatenated string = "abcabcd" - arranged as palindrome -> "abcdcba"
+     * 2.{"abc", "bc"}, concatenated string = "abcbc" - arranged as palindrome -> "bcacb"
+     * 3.{"abcd", "abc"}, concatenated string = "abcdadc" - arranged as palindrome -> "acdbdca"
+     *
+     * ->return 3, the number of complementary paris;
+     * */
     public static long getMaximumEvenSum(List<Integer> val) {
         long res = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
