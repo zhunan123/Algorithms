235211d3dcf7173f1aa7ed9062c0b870092f7752
zhunan123 <zhunan@bu.edu>
2023-02-08 10:32:16 +0800

    refactor sizeof in array

--- CS521/week3/null.c
+++ CS521/week3/null.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <string.h>;
+#include <string.h>
 
 int main(void) {
    char y[] = "HelloWorld my password is: hunter2";
-   char x[5] = {'H', 'E', 'L', 'L', 'O'};
+   char x[5] = {'H', 'E', 'L', 'L', 'O'}; //character has size of 1 so when sizeof(x) is 5
    char z[200] = "World  why is this happening"; //just gonna be the stirng since theres a termintor(0\) in the end
+   int b[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; //size of a int array is the size of int(4) * size of array, in this case sizeof(b) = 10 * 4 = 40
 
-   printf("The sizes of y, x, z are: %zu %zu %zu\n", sizeof(y), sizeof(x), sizeof(z));
+
+   printf("The sizes of y, x, z are: %zu %zu %zu %zd\n", sizeof(y), sizeof(x), sizeof(z), sizeof(b));
    printf("%s\n", x);
 
    printf("%s\n", z); //if the array size is exceeding it still will just print the string;
