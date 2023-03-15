2c2683b384e752205940ff7563d41f865d6a245c
zhunan123 <zhunan@bu.edu>
2023-03-15 10:40:29 -0700

    update shouldU mock

--- ShoulderU/Mock.java
+++ ShoulderU/Mock.java
@@ -31,12 +31,35 @@ public class Mock {
         return res;
     }
 
+    /* ----2---- */
+    /**
+     * 2.Merge Intervals
+     * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+     * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+     *
+     * Example 1:
+     * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]Output: [[1,6],[8,10],[15,18]]Explanation:
+     * Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+     * Example 2:
+     * Input: intervals = [[1,4],[4,5]]Output: [[1,5]]Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+     * */
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        System.out.println(Arrays.toString(intervals));
+        return intervals;
+    }
+
 
     public static void main(String[] args) {
         Mock mk = new Mock();
         int[] arr = {5, 7, 7, 8, 8, 10};
         int target = 8;
 
-        mk.findStartEndPos(arr, target);
+        int[][] intervals1 = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] intervals2 = {{1, 4}, {4, 5}};
+
+//        mk.findStartEndPos(arr, target);
+        mk.merge(intervals1);
     }
 }
