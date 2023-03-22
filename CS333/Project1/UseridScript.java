a0a34ceef9ab653d5655dd78d44007ae46a20b36
zhunan123 <zhunan@bu.edu>
2023-03-21 17:25:58 -0700

    update useridScript

--- CS333/Project1/UseridScript.java
+++ CS333/Project1/UseridScript.java
@@ -30,10 +30,36 @@ public class UseridScript {
     }
   }
 
+  public void generateUniqieID(String fname) {
+    File file = new File("unique_tags.txt");
+    BufferedReader br;
+    try {
+      FileWriter fw = new FileWriter(file);
+      br = new BufferedReader(new FileReader(fname));
+      String line = br.readLine();
+      HashSet<String> hs = new HashSet<String>();
+
+      while (line != null) {
+        String[] strArr = line.split(":");
+        if(hs.add(strArr[0])){
+          fw.write(strArr[0]);
+          fw.write("\n");
+        }
+        line = br.readLine();
+      }
+
+      br.close();
+      fw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
   public static void main(String[] args) {
     UseridScript us = new UseridScript();
     final String dir = System.getProperty("user.dir");
     System.out.println("current dir = " + dir);
-    us.start("ratings.txt");
+//    us.start("ratings.txt");
+    us.generateUniqieID("updated_tags.txt");
   }
 }
