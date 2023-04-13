bbc739f8a8294c205a42453f2ee54cb17757afe3
zhunan123 <zhunan@bu.edu>
2023-04-13 01:57:13 -0700

    added graph two sum

---
+++ ShoulderU/Array/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,43 @@
+package ShoulderU.Array;
+
+public class TwoSumIIInputArrayIsSorted {
+
+    /**
+     * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+     *
+     * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+     *
+     * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+     *
+     * Your solution must use only constant extra space.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: numbers = [2,7,11,15], target = 9
+     * Output: [1,2]
+     * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+     * */
+
+    /**
+     * using two pointer so first pointer at beginning, second pointer at the end, if numbers[start] + numbers[end] > target, end --,
+     * if numbers[start] + numbers[end] < target, start++, till we found the anwer
+     * */
+
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                break;
+            } else if (numbers[start] + numbers[end] > target) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return new int[] {start + 1, end + 1};
+    }
+}
