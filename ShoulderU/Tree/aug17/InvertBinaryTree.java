b341fce3fcda87f288895a71cd596afb6e0bfbea
zhunan123 <zhunan@bu.edu>
2023-08-18 22:02:03 -0700

    add leetcode daily BST 8/19

---
+++ ShoulderU/Tree/aug17/InvertBinaryTree.java
@@ -0,0 +1,16 @@
+package ShoulderU.Tree.aug17;
+
+import ShoulderU.Tree.TreeNode;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
