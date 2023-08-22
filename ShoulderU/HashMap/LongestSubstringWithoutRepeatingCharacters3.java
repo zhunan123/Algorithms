f9de13fee73e01cfa06d37a0449f21678ada5e81
zhunan123 <zhunan@bu.edu>
2023-08-21 23:21:33 -0700

    updated daily leet code 8.21 sliding window

--- ShoulderU/HashMap/LongestSubstringWithoutRepeatingCharacters3.java
+++ ShoulderU/HashMap/LongestSubstringWithoutRepeatingCharacters3.java
@@ -3,9 +3,47 @@ package ShoulderU.HashMap;
 import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters3 {
+  // use sliding window
+  // have two pointer, both set at begining, have a maxLength varible to keep track of window size, one pointer1 goes right until we find a same character, then need to update maxLength by doing compare setsize() and maxLngth
+  //then if already in set so remove the current char and advance left
+
+  /** #2 butter solution more understandable*/
   public int lengthOfLongestSubstring(String s) {
-    // have two pointer, both set at begining, have a maxLength varible to keep track of window size, one pointer1 goes right until we find a same character, then need to update maxLength by doing compare setsize() and maxLngth
-    //then if already in set so remove the current char and advance left
+    if (s == null || s.length() == 0) {
+      return 0;
+    }
+    int maxLength = 0;
+    int left = 0, right = 0;
+    int length = 0;
+    HashSet<Character> set = new HashSet<>();
+    while (right < s.length()) {
+      // if not in the set, we add to set, move right pointer, add length
+      if (!set.contains(s.charAt(right))) {
+        set.add(s.charAt(right));
+        right++;
+        length++;
+      } else {
+        // if its already in the set, we update the length to maxLength if greater,
+        // then we delete left pointer char in set(delete first char)
+        // advance left
+        // decrement length by one
+        if (length > maxLength) {
+          maxLength = length;
+        }
+        set.remove(s.charAt(left));
+        left++;
+        // then need to decrement the length since since we removed the left
+        // this is key
+        length--;
+      }
+    }
+    if (length > maxLength) {
+      maxLength = length;
+    }
+
+    return maxLength;
+  }
+  public int lengthOfLongestSubstring2(String s) {
     if (s == null || s.length() == 0) {
       return 0;
     }
