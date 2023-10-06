20a4fc10db50898877e5e1364f180a913748f977
zhunan123 <zhunan@bu.edu>
2023-10-05 22:46:26 -0700

    added 601 daily leetcode recap DP

--- ShoulderU/DP/MaximalSquare221.java
+++ ShoulderU/DP/MaximalSquare221.java
@@ -27,4 +27,24 @@ public class MaximalSquare221 {
         }
         return max * max;
     }
+
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int dp[][] = new int[m + 1][n + 1];
+        int max = 0;
+
+        for (int i = 1; i <= m; i++) { // need to make sure i <= m since we adding one more col and rol before matrix
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1'){ // at least 1 for erea since its self is a square
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+        return max * max;
+    }
 }
