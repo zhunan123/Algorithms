c223b3799043c913d96e8e998186723efff0c078
zhunan123 <zhunan@bu.edu>
2023-11-18 16:47:35 -0800

    added 603 leetcode coin change

--- CS603/CoinChange322.java
+++ CS603/CoinChange322.java
@@ -1,4 +1,24 @@
 package CS603;
 
+import java.util.Arrays;
+
 public class CoinChange322 {
+    public int coinChange(int[] coins, int amount) {
+        // https://www.youtube.com/watch?v=aQhNCYN5TsU
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < dp.length; i++) { // dp[i] is min # of coins to make up for value i
+            for (int coin : coins) {
+                // i - coin not dp[i] - coin
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+    }
 }
